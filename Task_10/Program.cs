102597c0ecfa6421191176ca8a60cef1010dca1c
LupuDan <110228686+LupuDan@users.noreply.github.com>
2022-08-25 14:08:47 +0300

    Add files via upload

---
+++ Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿int Number = 0;
+Console.WriteLine("Введите трёхзначное число: " );
+Number = Convert.ToInt32(Console.ReadLine());;
+if (Number >= 100 && Number <= 999)
+{
+    int DecreaseByTen = Number / 10;
+    int LastDigit = DecreaseByTen % 10;
+    Console.WriteLine("Вторая цифра числа = " + LastDigit);
+}
+else
+{
+    Console.WriteLine("Это не трёхзначное число");
+}
